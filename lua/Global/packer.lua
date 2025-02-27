e5f4617d01eba1745d1487429cd38cb4903b7647
RanchMonster <RanchMonster06@outlook.com>
2025-02-26 18:36:54 -1000

    I made it to where all lsps autoconfig so we don't have to config every lsp we use which is okay when you want custom stuff but for now I am not doing that so I don't care

--- lua/Global/packer.lua
+++ lua/Global/packer.lua
@@ -2,96 +2,78 @@ vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
    -- Packer can manage itself
+   use 'wbthomason/packer.nvim'
 
-   use {
+   -- LSP & Autocomplete
+   use 'neovim/nvim-lspconfig'
+   use 'williamboman/mason.nvim'
+   use 'williamboman/mason-lspconfig.nvim'
+   use 'hrsh7th/cmp-nvim-lsp'
+   use 'hrsh7th/cmp-buffer'
+   use 'hrsh7th/cmp-path'
+   use 'hrsh7th/cmp-cmdline'
+   use 'hrsh7th/nvim-cmp'
+   use 'hrsh7th/vim-vsnip'
+   use 'stevanmilic/nvim-lspimport'
+
+   -- Additional Plugins
+   use "github/copilot.vim"
+   use "tpope/vim-surround"
+   use { "folke/lazydev.nvim", ft = "lua" }
+   use { "nvim-lualine/lualine.nvim", requires = { 'nvim-tree/nvim-web-devicons', opt = true } }
+   use "ThePrimeagen/vim-be-good"
+   use { 'nvim-telescope/telescope.nvim', tag = '0.1.8', requires = { 'nvim-lua/plenary.nvim' } }
+   use "theprimeagen/harpoon"
+   use "mbbill/undotree"
+   use "tpope/vim-fugitive"
+
+   -- Treesitter
+   use { "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" }
+   use "nvim-treesitter/playground"
+      use {
       "ThePrimeagen/refactoring.nvim",
       requires = {
          { "nvim-lua/plenary.nvim" },
          { "nvim-treesitter/nvim-treesitter" }
       }
    }
-   use 'hrsh7th/cmp-nvim-lsp'
-   use 'hrsh7th/cmp-buffer'
-   use 'hrsh7th/cmp-path'
-   use 'hrsh7th/cmp-cmdline'
-   use 'hrsh7th/nvim-cmp'
-   use {"Mofiqul/vscode.nvim",
+
+   -- Themes
+   use { "Mofiqul/vscode.nvim", config = function() vim.cmd("colorscheme vscode") end }
+   use "xiyaowong/transparent.nvim"
+   use { "rose-pine/neovim", as = "rose-pine" }
+   use { "lukas-reineke/virt-column.nvim", opts = { char = "a", virtcolumn = "10", hilight = { "NonText" } } }
+
+   -- Mason & Auto LSP Setup
+   use {
+      "williamboman/mason.nvim",
+      run = ":MasonUpdate",
       config = function()
-         vim.cmd("colorscheme vscode")
-      end
-      }
-   use 'hrsh7th/vim-vsnip' -- I added this as it is needed for most lsp autocomplete which is something I think we both *should* use
-   use "Mofiqul/vscode.nvim"
-   use "github/copilot.vim"
-   use "tpope/vim-surround"
-   use { "folke/lazydev.nvim",
-      ft = "lua",
-      opts = {
-         library = {
-            {
-               path = "${3rd}/luv/library",
-               words = { "vim%.uv" }
-            },
-         },
-      },
-   }
-   use 'stevanmilic/nvim-lspimport'
-   use { "neovim/nvim-lspconfig",
-      config = function()
-         local capabilities = require("cmp_nvim_lsp").get_lsp_capabilities()
-         require("lspconfig").basedpyright.setup { capabilities = capabilities }
-         require("lspconfig").lua_ls.setup { capabilities = capabilities }
-         require("lspconfig").rust_analyzer.setup { capabilities = capabilities }
-         vim.api.nvim_create_autocmd("LspAttach", {
-            callback = function(args)
-               local c = vim.lsp.get_client_by_id(args.data.client_id)
-               if not c then return end
-               if c.supports_method("textDocument/formatting") then
-                  vim.api.nvim_create_autocmd("BufWritePre", {
-                     buffer = args.buf,
-                     callback = function()
-                        vim.lsp.buf.format({ bufnr = args.buf, id = c.id })
-                     end,
-                  })
-               end
+         require("mason").setup()
+         require("mason-lspconfig").setup {
+            automatic_installation = true,
+         }
+
+         local lspconfig = require("lspconfig")
+         local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+         require("mason-lspconfig").setup_handlers {
+            function(server_name) -- Auto-setup all installed LSPs
+               lspconfig[server_name].setup {
+                  capabilities = capabilities,
+                  on_attach = function(client, bufnr)
+                     if client.supports_method("textDocument/formatting") then
+                        vim.api.nvim_create_autocmd("BufWritePre", {
+                           buffer = bufnr,
+                           callback = function()
+                              vim.lsp.buf.format({ bufnr = bufnr })
+                           end,
+                        })
+                     end
+                  end,
+               }
             end,
-         })
+         }
       end,
    }
-   use {
-      'nvim-lualine/lualine.nvim',
-      requires = { 'nvim-tree/nvim-web-devicons', opt = true }
-   }
-   use 'wbthomason/packer.nvim'
-
-   use {
-      'williamboman/mason.nvim',
-      run = ':MasonUpdate' -- Optional: run :MasonUpdate after installation/update
-   }
-   use "ThePrimeagen/vim-be-good"
-   use {
-      'nvim-telescope/telescope.nvim', tag = '0.1.8',
-      -- or                            , branch = '0.1.x',
-      requires = { { 'nvim-lua/plenary.nvim' } }
-   }
-   use({ "xiyaowong/transparent.nvim",
-   })
-   use({
-      "rose-pine/neovim",
-      as = "rose-pine",
-   })
-   use({
-      "lukas-reineke/virt-column.nvim",
-      opts =
-      {
-         char = "a",
-         virtcolumn = "10",
-         hilight = { "NonText" }
-      }
-   })
-   use("nvim-treesitter/nvim-treesitter", { run = ":TSUpdate" })
-   use("nvim-treesitter/playground")
-   use("theprimeagen/harpoon")
-   use("mbbill/undotree")
-   use("tpope/vim-fugitive")
 end)
