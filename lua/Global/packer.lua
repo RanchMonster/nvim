14c7b1ef548dab3cf071340bc7f9df93811fbb86
J-Kistner <jk.gm.act@gmail.com>
2025-03-04 21:41:46 +0000

    Commit for testing

--- lua/Global/packer.lua
+++ lua/Global/packer.lua
@@ -3,6 +3,24 @@ vim.cmd [[packadd packer.nvim]]
 return require('packer').startup(function(use)
    -- Packer can manage itself
 
+   use "nvim-tree/nvim-web-devicons" -- optional, for file icons
+   use { "prichrd/netrw.nvim",
+      config = function()
+         require("netrw").setup({
+           -- File icons to use when `use_devicons` is false or if
+           -- no icon is found for the given file type.
+           icons = {
+             symlink = '',
+             directory = '',
+             file = '',
+           },
+           -- Uses mini.icon or nvim-web-devicons if true, otherwise use the file icon specified above
+           use_devicons = true,
+           mappings = { },
+         })
+      end
+   }
+
    use {
       "ThePrimeagen/refactoring.nvim",
       requires = {
