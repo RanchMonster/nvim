e938cf39dee3154c039d84f9d2054d4937469a9b
RanchMonster <RanchMonster06@outlook.com>
2025-03-09 12:32:56 -1000

    I fixed a bug in the lsp loader sometimes it starts the lsps before the lsp client in nvim was ready for it so I changed it to start the lsps when a file is opened (load and start are two different things by the way so the lsps are still lazy loaded)

--- lua/Global/packer.lua
+++ lua/Global/packer.lua
@@ -1,122 +1,122 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-	-- Packer can manage itself
-	use 'wbthomason/packer.nvim'
+   -- Packer can manage itself
+   use 'wbthomason/packer.nvim'
 
 
-	-- LSP & Autocomplete
-	use 'neovim/nvim-lspconfig'
-	use 'williamboman/mason.nvim'
-	use 'williamboman/mason-lspconfig.nvim'
-	use 'hrsh7th/cmp-nvim-lsp'
-	use 'hrsh7th/cmp-buffer'
-	use 'hrsh7th/cmp-path'
-	use 'hrsh7th/cmp-cmdline'
-	use 'hrsh7th/nvim-cmp'
-	use 'hrsh7th/vim-vsnip'
-	use 'stevanmilic/nvim-lspimport'
+   -- LSP & Autocomplete
+   use 'neovim/nvim-lspconfig'
+   use 'williamboman/mason.nvim'
+   use 'williamboman/mason-lspconfig.nvim'
+   use 'hrsh7th/cmp-nvim-lsp'
+   use 'hrsh7th/cmp-buffer'
+   use 'hrsh7th/cmp-path'
+   use 'hrsh7th/cmp-cmdline'
+   use 'hrsh7th/nvim-cmp'
+   use 'hrsh7th/vim-vsnip'
+   use 'stevanmilic/nvim-lspimport'
 
-	-- Additional Plugins
-	use "github/copilot.vim"
-	use "tpope/vim-surround"
-	use { "folke/lazydev.nvim", ft = "lua" }
-	use { "nvim-lualine/lualine.nvim", requires = { 'nvim-tree/nvim-web-devicons', opt = true } }
-	use "ThePrimeagen/vim-be-good"
-	use { 'nvim-telescope/telescope.nvim', tag = '0.1.8', requires = { 'nvim-lua/plenary.nvim' } }
-	use "theprimeagen/harpoon"
-	use "mbbill/undotree"
-	use "tpope/vim-fugitive"
+   -- Additional Plugins
+   use "github/copilot.vim"
+   use "tpope/vim-surround"
+   use { "folke/lazydev.nvim", ft = "lua" }
+   use { "nvim-lualine/lualine.nvim", requires = { 'nvim-tree/nvim-web-devicons', opt = true } }
+   use "ThePrimeagen/vim-be-good"
+   use { 'nvim-telescope/telescope.nvim', tag = '0.1.8', requires = { 'nvim-lua/plenary.nvim' } }
+   use "theprimeagen/harpoon"
+   use "mbbill/undotree"
+   use "tpope/vim-fugitive"
 
-	-- Treesitter
-	use { "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" }
-	use "nvim-treesitter/playground"
-	use {
-		"ThePrimeagen/refactoring.nvim",
-		requires = {
-			{ "nvim-lua/plenary.nvim" },
-			{ "nvim-treesitter/nvim-treesitter" }
-		}
-	}
+   -- Treesitter
+   use { "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" }
+   use "nvim-treesitter/playground"
+   use {
+      "ThePrimeagen/refactoring.nvim",
+      requires = {
+         { "nvim-lua/plenary.nvim" },
+         { "nvim-treesitter/nvim-treesitter" }
+      }
+   }
 
 
-	-- Themes
-	use { "Mofiqul/vscode.nvim", config = function() vim.cmd("colorscheme vscode") end }
-	use "xiyaowong/transparent.nvim"
-	use { "rose-pine/neovim", as = "rose-pine" }
-	use { "lukas-reineke/virt-column.nvim", opts = { char = "a", virtcolumn = "10", hilight = { "NonText" } } }
+   -- Themes
+   use { "Mofiqul/vscode.nvim", config = function() vim.cmd("colorscheme vscode") end }
+   use "xiyaowong/transparent.nvim"
+   use { "rose-pine/neovim", as = "rose-pine" }
+   use { "lukas-reineke/virt-column.nvim", opts = { char = "a", virtcolumn = "10", hilight = { "NonText" } } }
 
-	-- Mason & Auto LSP Setup
-	use 'hrsh7th/cmp-nvim-lsp'
-	use 'hrsh7th/cmp-buffer'
-	use 'hrsh7th/cmp-path'
-	use 'hrsh7th/cmp-cmdline'
-	use 'hrsh7th/nvim-cmp'
-	use 'hrsh7th/vim-vsnip' -- I added this as it is needed for most lsp autocomplete which is something I think we both *should* use
-	use "Mofiqul/vscode.nvim"
-	use "github/copilot.vim"
-	use "tpope/vim-surround"
-	use { "folke/lazydev.nvim",
-		ft = "lua",
-		opts = {
-			library = {
-				{
-					path = "${3rd}/luv/library",
-					words = { "vim%.uv" }
-				},
-			},
-		},
-	}
-	-- Packer can manage itself
-	use 'wbthomason/packer.nvim'
+   -- Mason & Auto LSP Setup
+   use 'hrsh7th/cmp-nvim-lsp'
+   use 'hrsh7th/cmp-buffer'
+   use 'hrsh7th/cmp-path'
+   use 'hrsh7th/cmp-cmdline'
+   use 'hrsh7th/nvim-cmp'
+   use 'hrsh7th/vim-vsnip' -- I added this as it is needed for most lsp autocomplete which is something I think we both *should* use
+   use "Mofiqul/vscode.nvim"
+   use "github/copilot.vim"
+   use "tpope/vim-surround"
+   use { "folke/lazydev.nvim",
+      ft = "lua",
+      opts = {
+         library = {
+            {
+               path = "${3rd}/luv/library",
+               words = { "vim%.uv" }
+            },
+         },
+      },
+   }
+   -- Packer can manage itself
+   use 'wbthomason/packer.nvim'
 
-	-- Icons and file management
-	use "nvim-tree/nvim-web-devicons"
-	use { "prichrd/netrw.nvim",
-		config = function()
-			require("netrw").setup({
-				icons = {
-					symlink = '',
-					directory = '',
-					file = '',
-				},
-				use_devicons = true,
-				mappings = {},
-			})
-		end
-	}
+   -- Icons and file management
+   use "nvim-tree/nvim-web-devicons"
+   use { "prichrd/netrw.nvim",
+      config = function()
+         require("netrw").setup({
+            icons = {
+               symlink = '',
+               directory = '',
+               file = '',
+            },
+            use_devicons = true,
+            mappings = {},
+         })
+      end
+   }
 
-	-- LSP & Autocomplete
-	use 'neovim/nvim-lspconfig'
-	use 'williamboman/mason.nvim'
-	use 'williamboman/mason-lspconfig.nvim'
-	use 'hrsh7th/cmp-nvim-lsp'
-	use 'hrsh7th/cmp-buffer'
-	use 'hrsh7th/cmp-path'
-	use 'hrsh7th/cmp-cmdline'
-	use 'hrsh7th/nvim-cmp'
-	use 'hrsh7th/vim-vsnip'
-	use 'stevanmilic/nvim-lspimport'
+   -- LSP & Autocomplete
+   use 'neovim/nvim-lspconfig'
+   use 'williamboman/mason.nvim'
+   use 'williamboman/mason-lspconfig.nvim'
+   use 'hrsh7th/cmp-nvim-lsp'
+   use 'hrsh7th/cmp-buffer'
+   use 'hrsh7th/cmp-path'
+   use 'hrsh7th/cmp-cmdline'
+   use 'hrsh7th/nvim-cmp'
+   use 'hrsh7th/vim-vsnip'
+   use 'stevanmilic/nvim-lspimport'
 
-	-- Additional Plugins
-	use "github/copilot.vim"
-	use "tpope/vim-surround"
-	use { "folke/lazydev.nvim", ft = "lua" }
-	use { "nvim-lualine/lualine.nvim", requires = { 'nvim-tree/nvim-web-devicons', opt = true } }
-	use "ThePrimeagen/vim-be-good"
-	use { 'nvim-telescope/telescope.nvim', tag = '0.1.8', requires = { 'nvim-lua/plenary.nvim' } }
-	use "theprimeagen/harpoon"
-	use "mbbill/undotree"
-	use "tpope/vim-fugitive"
+   -- Additional Plugins
+   use "github/copilot.vim"
+   use "tpope/vim-surround"
+   use { "folke/lazydev.nvim", ft = "lua" }
+   use { "nvim-lualine/lualine.nvim", requires = { 'nvim-tree/nvim-web-devicons', opt = true } }
+   use "ThePrimeagen/vim-be-good"
+   use { 'nvim-telescope/telescope.nvim', tag = '0.1.8', requires = { 'nvim-lua/plenary.nvim' } }
+   use "theprimeagen/harpoon"
+   use "mbbill/undotree"
+   use "tpope/vim-fugitive"
 
-	-- Treesitter
-	use { "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" }
-	use "nvim-treesitter/playground"
-	use { "ThePrimeagen/refactoring.nvim", requires = { "nvim-lua/plenary.nvim", "nvim-treesitter/nvim-treesitter" } }
+   -- Treesitter
+   use { "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" }
+   use "nvim-treesitter/playground"
+   use { "ThePrimeagen/refactoring.nvim", requires = { "nvim-lua/plenary.nvim", "nvim-treesitter/nvim-treesitter" } }
 
-	-- Themes
-	use { "Mofiqul/vscode.nvim", config = function() vim.cmd("colorscheme vscode") end }
-	use "xiyaowong/transparent.nvim"
-	use { "rose-pine/neovim", as = "rose-pine" }
-	use { "lukas-reineke/virt-column.nvim", opts = { char = "a", virtcolumn = "10", hilight = { "NonText" } } }
+   -- Themes
+   use { "Mofiqul/vscode.nvim", config = function() vim.cmd("colorscheme vscode") end }
+   use "xiyaowong/transparent.nvim"
+   use { "rose-pine/neovim", as = "rose-pine" }
+   use { "lukas-reineke/virt-column.nvim", opts = { char = "a", virtcolumn = "10", hilight = { "NonText" } } }
 end)
