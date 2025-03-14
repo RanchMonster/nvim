57f18232de9541f602fa3a0bf187749f7dc4bc2b
J-Kistner <jk.gm.act@gmail.com>
2025-03-14 00:06:16 +0000

    feat: Added auto filetype support for php

--- lua/Global/lsp/filetypes.lua
+++ lua/Global/lsp/filetypes.lua
@@ -1,10 +1,17 @@
-local autofiletype = vim.api.nvim_create_augroup( "autofiletype", { clear = true })
-vim.api.nvim_create_autocmd( { "BufEnter" }, { 
-	pattern = "*.slint",
-	group = autofiletype,
-	callback = function()
-		print( "Setting filetype to slint" )
-		vim.cmd( "set filetype=slint" )
-	end
+local autofiletype = vim.api.nvim_create_augroup("autofiletype", { clear = true })
+vim.api.nvim_create_autocmd({ "BufEnter" }, {
+   pattern = "*.slint",
+   group = autofiletype,
+   callback = function()
+      vim.cmd("set filetype=slint")
+   end
+}
+)
+vim.api.nvim_create_autocmd({ "BufEnter" }, {
+   pattern = { "*.php" },
+   group = autofiletype,
+   callback = function()
+      vim.cmd("set filetype=php")
+   end
 }
 )
