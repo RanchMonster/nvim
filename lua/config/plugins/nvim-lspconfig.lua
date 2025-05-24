0fb19a711659299542c80b340b2d2c2f88851fde
J-Kistner <jk.gm.act@gmail.com>
2025-05-24 02:55:24 -0400

    Added: basic lsp keymaps

--- lua/config/plugins/nvim-lspconfig.lua
+++ lua/config/plugins/nvim-lspconfig.lua
@@ -1,18 +1,53 @@
-return {{
+return { {
    "neovim/nvim-lspconfig",
-   dependencies = {{
+   dependencies = { {
       "folke/lazydev.nvim",
-      ft ="lua",
-      opts = { 
+      ft = "lua",
+      opts = {
          library = {
             { path = "${3rd}/luv/library", words = { "vim%.uv" } },
          },
       },
-   }},
+   } },
    config = function()
+      -- For inline error messages
+      vim.diagnostic.config({
+         virtual_text = true, -- show inline text
+         signs = true,        -- show signs in gutter
+         underline = true,    -- underline errors
+         update_in_insert = false,
+         severity_sort = true,
+      })
+
       -- Init Lsps --
-      require( "lspconfig" ).lua_ls.setup {}
-      require( "lspconfig" ).basedpyright.setup {}
-      require( "lspconfig" ).rust_analyzer.setup {}
+      require("lspconfig").lua_ls.setup {}
+      require("lspconfig").basedpyright.setup {}
+      require("lspconfig").rust_analyzer.setup {}
+
+      -- Lsp Key Maps
+      l = vim.lsp.buf
+      Key("n", "<leader>gh", l.hover, "( Lsp ) Show Hover")
+      Key("n", "<leader>gd", l.definition, "( Lsp ) Go to Definition")
+      Key("n", "<leader>fr", l.references, "( Lsp ) Find Refrences")
+
+      vim.api.nvim_create_autocmd("LspAttach", {
+         callback = function(args)
+            -- Get the current lsp client
+            local client = vim.lsp.get_client_by_id(args.data.client_id)
+            if not client then return end
+            -- Checking if there is an lsp for the language
+            -- The next line was made using "gra"
+            ---@diagnostic disable-next-line: missing-parameter, param-type-mismatch
+            if client.supports_method("textDocument/formatting") then
+               vim.api.nvim_create_autocmd("BufWritePre", { -- Autocmd: When atempting to write stack( this, write )
+                  buffer = args.buf,                        -- args.buf: source of current buffer
+                  callback = function()
+                     -- Formats the current buffer, using the current lsp
+                     vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
+                  end
+               })
+            end
+         end,
+      })
    end,
-}}
+} }
--- lua/mconfig/remaps.lua
+++ lua/mconfig/remaps.lua
@@ -2,52 +2,51 @@
 
 
 -- EzKey
-function Key( mode, key, map, desc )
+function Key(mode, key, map, desc)
    desc = desc or ""
    modes = {}
    for i = 1, #mode do
-      modes[i] = mode:sub(i,i)
+      modes[i] = mode:sub(i, i)
    end
-	vim.keymap.set( modes, key, map, { desc = desc })
+   vim.keymap.set(modes, key, map, { desc = desc })
 end
 
 -- File Navigation
-Key( "n", "<Leader>ft", function() vim.cmd( "Ex" ) end, "Opens the file tree." )  
+Key("n", "<Leader>ft", function() vim.cmd("Ex") end, "Opens the file tree.")
 
 -- Better Indentation
-Key( "i", "<Tab>", function() vim.cmd( "norm >>" ) end, "Indent" )
-Key( "i", "<S-Tab>", function() vim.cmd( "norm <<" ) end, "Unindent" )
-Key( "vn", "<Tab>", ">", "Indent" ) 
-Key( "vn", "<S-Tab>", "<", "Unindent" ) 
+Key("i", "<Tab>", function() vim.cmd("norm >>") end, "Indent")
+Key("i", "<S-Tab>", function() vim.cmd("norm <<") end, "Unindent")
+Key("vn", "<Tab>", ">", "Indent")
+Key("vn", "<S-Tab>", "<", "Unindent")
 
--- Generil Util 
-Key( "vi", "<C-c>", "<Esc>", "Allows for <C-c> to exit multiline." ) 
-Key( "n", "<leader><leader>", function() 
-      vim.cmd( "so" ) 
-      vim.cmd( "Lazy" )
-   end, "Refreshes the config and the current file." ) 
-Key("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], "Replace" )
+-- Generil Util
+Key("vi", "<C-c>", "<Esc>", "Allows for <C-c> to exit multiline.")
+Key("n", "<leader><leader>", function()
+   vim.cmd("so")
+   vim.cmd("Lazy")
+end, "Refreshes the config and the current file.")
+Key("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], "Replace")
 
--- Formatting 
-Key( "n", "<leader>fo", vim.lsp.buf.format, "Formats the file using the lsp." ) 
+-- Formatting
+Key("n", "<leader>fo", vim.lsp.buf.format, "Formats the file using the lsp.")
 
--- Navigation  
-Key( "n", "E", "2be", "Moves to the end of the last word." )
+-- Navigation
+Key("n", "E", "2be", "Moves to the end of the last word.")
 Key("v", "J", ":m '>+1<CR>gv=gv", "Move block in visual mode")
 Key("v", "K", ":m '<-2<CR>gv=gv", "Move block in visual mode")
-Key("n", "J", "mzJ`z", "Cursor stays in place when merging lines" )
-Key("n", "K", "mzJ`z", "Cursor stays in place when merging lines" )
-Key( "n", "<C-d>", "<C-d>zz", "Center screen after half page jump. " )
-Key( "n", "<C-u>", "<C-u>zz", "Center screen after half page jump. " )
-Key("n", "n", "nzzzv", "Center after find next" )
-Key("n", "N", "Nzzzv", "Center after find next" )
-Key("n", "<C-k>", "<cmd>cnext<CR>zz", "Random Jump idrk" )
-Key("n", "<C-j>", "<cmd>cprev<CR>zz", "Random Jump idrk" )
-Key("n", "<leader>k", "<cmd>lnext<CR>zz", "Random Jump idrk" )
-Key("n", "<leader>j", "<cmd>lprev<CR>zz", "Random Jump idrk" )
+Key("n", "J", "mzJ`z", "Cursor stays in place when merging lines")
+Key("n", "K", "mzJ`z", "Cursor stays in place when merging lines")
+Key("n", "<C-d>", "<C-d>zz", "Center screen after half page jump. ")
+Key("n", "<C-u>", "<C-u>zz", "Center screen after half page jump. ")
+Key("n", "n", "nzzzv", "Center after find next")
+Key("n", "N", "Nzzzv", "Center after find next")
+Key("n", "<C-k>", "<cmd>cnext<CR>zz", "Random Jump idrk")
+Key("n", "<C-j>", "<cmd>cprev<CR>zz", "Random Jump idrk")
+Key("n", "<leader>k", "<cmd>lnext<CR>zz", "Random Jump idrk")
+Key("n", "<leader>j", "<cmd>lprev<CR>zz", "Random Jump idrk")
 
 -- Better Bin Management
-Key( "nv", "<leader>d", "\"_d", "A delete that does not store the result." ) -- TODO: Learn to press backslash
-Key( "n", "<leader>P", "viwp", "Pastes inside the current word." )
-Key( "x", "<leader>p", [["_dp]], "Paste from trash buffer." )
-
+Key("nv", "<leader>d", "\"_d", "A delete that does not store the result.")   -- TODO: Learn to press backslash
+Key("n", "<leader>P", "viwp", "Pastes inside the current word.")
+Key("x", "<leader>p", [["_dp]], "Paste from trash buffer.")
