70a506e85764388325469cfcacaee5e92ff62c19
J-Kistner <jk.gm.act@gmail.com>
2025-05-25 02:15:47 -0400

    Fix: filetype selection for multi-grep

--- lua/config/Telescope/live_grep.lua
+++ lua/config/Telescope/live_grep.lua
@@ -1,57 +1,57 @@
+--
+-- tjdevries - advent of nvim
+--
+
 local pickers = require "telescope.pickers"
 local finders = require "telescope.finders"
 local make_entry = require "telescope.make_entry"
-local conf = require("telescope.config").values
-local Job = require "plenary.job"
+local conf = require "telescope.config".values
 
 local M = {}
 
 local live_multigrep = function(opts)
-  opts = opts or {}
-  opts.cwd = opts.cwd or vim.uv.cwd()
+   opts = opts or {}
+   opts.cwd = opts.cwd or vim.uv.cwd()
 
-  pickers.new(opts, {
-    prompt_title = "Multi Grep",
-    finder = finders.new_job(function(prompt)
-      if not prompt or prompt == "" then
-        return nil
-      end
+   local finder = finders.new_async_job {
+      command_generator = function(prompt)
+         if not prompt or prompt == "" then
+            return nil
+         end
 
-      -- Split by single space, not double space
-      local pieces = vim.split(prompt, " ", { plain = true })
+         local pieces = vim.split(prompt, "  ")
+         local args = { "rg" }
+         if pieces[1] then
+            table.insert(args, "-e")
+            table.insert(args, pieces[1])
+         end
 
-      local args = {
-        "--color=never",
-        "--no-heading",
-        "--with-filename",
-        "--line-number",
-        "--column",
-        "--smart-case",
-      }
+         if pieces[2] then
+            table.insert(args, "-g")
+            table.insert(args, pieces[2])
+         end
 
-      if #pieces >= 1 and pieces[1] ~= "" then
-        table.insert(args, "-e")
-        table.insert(args, pieces[1])
-      end
+         ---@diagnostic disable-next-line: deprecated
+         return vim.tbl_flatten {
+            args,
+            { "--color=never", "--no-heading", "--with-filename", "--line-number", "--column", "--smart-case" },
+         }
+      end,
+      entry_maker = make_entry.gen_from_vimgrep(opts),
+      cwd = ".",
+   }
 
-      if #pieces >= 2 then
-        table.insert(args, "-g")
-        table.insert(args, pieces[2])
-      end
-
-      return { "rg", unpack(args) }
-    end, make_entry.gen_from_vimgrep(opts), {
-      cwd = opts.cwd,
-    }),
-    previewer = conf.grep_previewer(opts),
-    sorter = conf.generic_sorter(opts),
-  }):find()
+   pickers.new(opts, {
+      debounce = 100,
+      prompt_title = "Multi Grep",
+      finder = finder,
+      previewer = conf.grep_previewer(opts),
+      sorter = require("telescope.sorters").empty(),
+   }):find()
 end
 
-M.setup = function() 
+M.setup = function()
    vim.keymap.set("n", "<C-g>", live_multigrep)
 end
-M.live_multigrep = live_multigrep
 
 return M
-
