5e1253118099b447750acc9fd51d38c5bb425863
J-Kistner <jk.gm.act@gmail.com>
2025-05-25 11:07:04 -0400

    Intentionally created error to test workflow

--- init.lua
+++ init.lua
@@ -3,7 +3,7 @@
 ---@param key string The keys to be pressed to trigger the bind
 ---@param map string|function What the map does, Do NOT pass in a function call
 ---@param desc string The description of the bind, NOT required
-function Key(mode, key, map, desc)
+Key(mode, key, map, desc)
    desc = desc or ""
    local modes = {}
    for i = 1, #mode do
