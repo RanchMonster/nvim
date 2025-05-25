36622e743dd3712d45087ebe90cd54f0965f40c6
J-Kistner <jk.gm.act@gmail.com>
2025-05-25 10:56:39 -0400

    Removed: Unused keybinds from init write

--- lua/config/plugins/telescope.lua
+++ lua/config/plugins/telescope.lua
@@ -17,17 +17,10 @@ return {
       -- Nav & Git
       require("telescope").load_extension("fzf")
       Key("n", "<leader>ff", builtin.find_files, "( Telescope ) Find Files")
-      -- Key( "n", "<C-g>", function()
-      --    builtin.live_grep( vim.input() )
-      -- end, "( Telescope ) Find Grep" )
       Key("n", "<leader>fh", builtin.help_tags, "( Telescope ) Find Help")
       Key("n", "<leader>fgf", builtin.git_files, "( Telescope ) Find Git Files")
       Key("n", "<leader>fgb", builtin.git_branches, "( Telescope ) Find Git Branches")
       Key("ni", "<C-l>", builtin.spell_suggest, "( Telescope ) Spell Suggest")
-
-      -- Lsp
-      -- Key("n", "<leader>gd", telescope.lsp_definitions, "( Telescope ) Go to Definition")
-      -- Key("n", "<leader>fr", telescope.lsp_references, "( Telescope ) Find Refrences")
 
       require("config.Telescope.live_grep").setup()
    end,
