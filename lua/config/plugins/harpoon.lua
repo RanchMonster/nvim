cfaac4a71f59be737b7aa55a94c911ed76635921
RanchMonster <ranchmonster06@outlook.com>
2025-07-18 19:03:18 +0100

    fixed harpon bug

--- lua/config/plugins/harpoon.lua
+++ lua/config/plugins/harpoon.lua
@@ -1,4 +1,3 @@
-UiOpen = false
 return {
    {
       "ThePrimeagen/harpoon",
@@ -6,18 +5,16 @@ return {
       dependencies = { "nvim-lua/plenary.nvim" },
       config = function()
          local harpoon = require("harpoon")
-         harpoon:setup()
+         local opts = {
+            settings = {
+               save_on_toggle = true,
+               sync_on_ui_close = true
+            }
+         }
+         harpoon:setup(opts)
          vim.api.nvim_create_augroup("harpoon2", { clear = true })
          Key("n", "<leader>a", function() harpoon:list():add() end, "Adds an entry to the harpoon list.")
-         Key("n", "<C-e>", function()
-            if UiOpen then
-               vim.cmd("w")
-               UiOpen = false
-            else
-               UiOpen = true
-            end
-            harpoon.ui:toggle_quick_menu(harpoon:list())
-         end, "Toggles the harpoon menu popup.")
+         Key("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, "Toggles the harpoon menu popup.")
          Key("n", "<C-h>", function() harpoon:list():select(1) end, "Adds an entry to the harpoon list")
          Key("n", "<C-t>", function() harpoon:list():select(2) end, "Adds an entry to the harpoon list")
          Key("n", "<C-n>", function() harpoon:list():select(3) end, "Adds an entry to the harpoon list")
