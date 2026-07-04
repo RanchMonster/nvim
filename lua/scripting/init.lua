7b42c86550bf6ecab57547848d65bb3a545127e3
Joshua <RanchMonster06@outlook.com>
2026-07-03 19:10:22 -0500

    removed unused status system I deciced I didn't want to add this feature

--- lua/scripting/init.lua
+++ lua/scripting/init.lua
@@ -3,4 +3,3 @@ require("scripting.remaps")
 require("scripting.snake_write") -- I will keep this but I never really use this
 require("scripting.cinit")       -- This needs to be overhauled
 require("scripting.autoupdate")  -- This dosen't actually work and also is kinda pointless now
-require("scripting.nvim-status")
